f0c67f53c5d0387fd70603e7fd789bcce081198f
henrilindeza27@gmail.com <henrilindeza27@gmail.com>
2023-03-13 19:36:57 +0000

    bonus

---
+++ libft/ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henrilindeza <henrilindeza@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 19:27:44 by henrilindez       #+#    #+#             */
+/*   Updated: 2023/03/13 19:34:22 by henrilindez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
