1e81b3fed83ca2d53e83aca0628ada18c0ffe419
henrilindeza27@gmail.com <henrilindeza27@gmail.com>
2023-03-13 17:36:55 +0000

    bonus

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henrilindeza <henrilindeza@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 17:27:45 by henrilindez       #+#    #+#             */
+/*   Updated: 2023/03/13 17:35:11 by henrilindez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
