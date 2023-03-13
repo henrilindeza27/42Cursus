7ff89a8893c71ca9d5e5aac6f7ef044434386daf
henrilindeza27@gmail.com <henrilindeza27@gmail.com>
2023-03-13 01:12:42 +0000

    Part2 StrMapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henrilindeza <henrilindeza@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 00:49:48 by henrilindez       #+#    #+#             */
+/*   Updated: 2023/03/13 01:00:10 by henrilindez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*newstr;
+
+	if (!s || !*f)
+		return (NULL);
+	newstr = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!newstr)
+		return (NULL);
+	i = -1;
+	while (s[++i])
+		newstr[i] = f(i, s[i]);
+	newstr[i] = '\0';
+	return (newstr);
+}
