f2d57602386392fd73588f670a77eef39c37ee7d
henrilindeza27@gmail.com <henrilindeza27@gmail.com>
2023-03-13 18:33:43 +0000

    bonus

---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henrilindeza <henrilindeza@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 18:19:20 by henrilindez       #+#    #+#             */
+/*   Updated: 2023/03/13 18:31:25 by henrilindez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
